dd462578a4ca0f5781f31941fc8ec7dc1ec7cfb7
manikantatholeti <83859605+manikantatholeti@users.noreply.github.com>
2023-08-27 19:20:05 +0530

    Update main.tf

--- jenkins/main.tf
+++ jenkins/main.tf
@@ -1,72 +1,72 @@
-# network module
-module "vpc" {
-    source ="../modules/vpc"
-}
-output "vpc_id" {
-  value = module.vpc.vpc_id
-  depends_on = [ 
-    module.vpc
-  ]
-}
-output "public_subnet_id" {
-  value = module.vpc.public_subnet_id
-  depends_on = [
-    module.vpc
-  ]
-}
-output "private_subnet_id" {
-  value = module.vpc.private_subnet_id
-  depends_on = [
-    module.vpc
-  ]
-}
+# # network module
+# module "vpc" {
+#     source ="../modules/vpc"
+# }
+# output "vpc_id" {
+#   value = module.vpc.vpc_id
+#   depends_on = [ 
+#     module.vpc
+#   ]
+# }
+# output "public_subnet_id" {
+#   value = module.vpc.public_subnet_id
+#   depends_on = [
+#     module.vpc
+#   ]
+# }
+# output "private_subnet_id" {
+#   value = module.vpc.private_subnet_id
+#   depends_on = [
+#     module.vpc
+#   ]
+# }
 
-# Security Group Module
-module "sg" {
-    source ="../modules/sg"
-    security_group_vpc_id = module.vpc.vpc_id
-    depends_on = [
-      module.vpc
-    ]
-}
+# # Security Group Module
+# module "sg" {
+#     source ="../modules/sg"
+#     security_group_vpc_id = module.vpc.vpc_id
+#     depends_on = [
+#       module.vpc
+#     ]
+# }
 
-output "sg_id" {
-  value = module.sg.sg_id
-  depends_on = [
-    module.sg
-  ]
-}
+# output "sg_id" {
+#   value = module.sg.sg_id
+#   depends_on = [
+#     module.sg
+#   ]
+# }
 
-module "iam_role" {
-  source = "../modules/iam_role"  
-}
+# module "iam_role" {
+#   source = "../modules/iam_role"  
+# }
 
-output "iam_instance_profile_name" {
-  value = module.iam_role.iam_instance_profile_name
-  depends_on = [
-    module.iam_role
-  ]
-}
+# output "iam_instance_profile_name" {
+#   value = module.iam_role.iam_instance_profile_name
+#   depends_on = [
+#     module.iam_role
+#   ]
+# }
 
-# Instance Module
-module "ec2_instance" {
-  source  = "../modules/ec2"
-  instancetype = "t3.micro"
-  ami = "ami-0f8ca728008ff5af4"
-  key_name = "mani.pem"
-  subnet_id = module.vpc.public_subnet_id
-  associate_public_ip_address = true
-  instance_security_group_id = [ module.sg.sg_id ]
-  iam_profile = module.iam_role.iam_instance_profile_name
-  Name = "test-instance"
-  # version = "~> 4.0"
-# 
-  depends_on = [
-    module.sg,
-    module.iam_role
-  ]
-}
-# # backend module
-# module "backend_s3" {
-#   source = "../modules/tfstate-s3" 
-#  }
+# # Instance Module
+# module "ec2_instance" {
+#   source  = "../modules/ec2"
+#   instancetype = "t3.micro"
+#   ami = "ami-0f8ca728008ff5af4"
+#   key_name = "mani.pem"
+#   subnet_id = module.vpc.public_subnet_id
+#   associate_public_ip_address = true
+#   instance_security_group_id = [ module.sg.sg_id ]
+#   iam_profile = module.iam_role.iam_instance_profile_name
+#   Name = "test-instance"
+#   # version = "~> 4.0"
+# # 
+#   depends_on = [
+#     module.sg,
+#     module.iam_role
+#   ]
+# }
+# # # backend module
+# # module "backend_s3" {
+# #   source = "../modules/tfstate-s3" 
+# #  }
